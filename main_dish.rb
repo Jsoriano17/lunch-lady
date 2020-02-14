f1aa2fa5c1fbeeed120cf9f9ca717b8a00f7d2aa
Jeffrey Soriano <jeffsor17@gmail.com>
2020-02-13 17:29:38 -0700

    added total amount class

--- main_dish.rb
+++ main_dish.rb
@@ -1,7 +1,29 @@
 class Main_dish
+    def initialize
+        menu
+    end
     def menu
+        puts '+++++++++++++++++++++++'
         puts '1) Pizza    $2.00 per slice' 
         puts '2) Cheese Burger      $3.00'
         puts '3) Burrito            $5.00'
+        puts '+++++++++++++++++++++++'
+        puts 'Please select a menu item'
+        menu_choice()
+    end
+    def menu_choice
+        menu_number = gets.to_i
+        case menu_number
+        when 1
+            puts 'you added Pizza to your order'
+        when 2
+            puts 'you added a Cheese Burger to your order'
+        when 3
+            puts 'you added a Burrito to your order'
+        else 
+        end
     end
 end
+testing = Main_dish.new
+puts testing
+
---
+++ total_amount.rb
@@ -0,0 +1,13 @@
+# i want the money they chose to go into an array with the tottal amount
+#the array will loop and add it all together
+
+class Total_amount
+    attr_accessor
+    def initialize
+    end
+    def items_ordered
+    end
+    def items_added
+    end
+
+end
